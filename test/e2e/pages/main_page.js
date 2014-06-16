6f79d86bd118e82e1678165b36df2a79c770fa27
Bastien Calou <bastien@blackbee-systems.com>
2014-06-17 01:11:34 +0200

    protractor tests with object page

---
+++ test/e2e/pages/main_page.js
@@ -0,0 +1,42 @@
+var mainPage = function() {
+
+  this.defaultLinesNb = 10;
+  this.defaultColumnsNb = 10;
+
+  this.cells = element.all(by.css('#grid input'));
+
+  this.linesNbInput = element(by.model('linesNb'));
+  this.columnsNbInput = element(by.model('columnsNb'));
+
+  this.line1Column1Cell = element(by.css('#grid tr:nth-child(1) td:nth-child(1) input'));
+  this.line1Column9Cell = element(by.css('#grid tr:nth-child(1) td:nth-child(9) input'));
+  this.line1Column10Cell = element(by.css('#grid tr:nth-child(1) td:nth-child(10) input'));
+  this.line2Column1Cell = element(by.css('#grid tr:nth-child(2) td:nth-child(1) input'));
+  this.line2Column3Cell = element(by.css('#grid tr:nth-child(2) td:nth-child(3) input'));
+  this.line4Column2Cell = element(by.css('#grid tr:nth-child(4) td:nth-child(2) input'));
+
+  this.saveGridButton = element(by.css("#save-grid"));
+  this.loadGridButton = element(by.css("#load-grid"));
+
+  this.getCell = function(line, column) {
+    return element(by.css('#grid tr:nth-child(' + line + ') td:nth-child(' + column + ') input'));
+  }
+
+  this.setLinesNumber = function(linesNb) {
+    this.linesNbInput.clear();
+    this.linesNbInput.sendKeys(linesNb);
+  }
+
+  this.setColumnsNumber = function(linesNb) {
+    this.columnsNbInput.clear();
+    this.columnsNbInput.sendKeys(linesNb);
+  }
+
+  this.resetDimensions = function() {
+    this.setLinesNumber(this.defaultLinesNb);
+    this.setColumnsNumber(this.defaultColumnsNb);
+  }
+
+};
+
+module.exports = new mainPage();
--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -4,115 +4,86 @@
 
 describe('gridigger', function() {
 
+  var mainPage = require('./pages/main_page.js');
+
   browser.get('index.html');
 
   it('should show a specific number of cells at the beginning', function() {
 
-    var cells = element.all(by.css('#grid input'));
-
     // Verifying there are 100 cells
-    expect(cells.count()).toBe(100);
+    expect(mainPage.cells.count()).toBe(100);
 
   });
 
   it('should change the number of cells when changing linesNb and columnsNb', function() {
 
-    var linesNb = element(by.model('linesNb'));
-    var columnsNb = element(by.model('columnsNb'));
-
     // Setting linesNb and columnsNb
-    linesNb.clear();
-    linesNb.sendKeys('4');
-    columnsNb.clear();
-    columnsNb.sendKeys('5');
+    mainPage.setLinesNumber('4');
+    mainPage.setColumnsNumber('5');
 
-    // There now should be linesNb*columnsNb cells
-    var cells = element.all(by.css('#grid input'));
-    expect(cells.count()).toBe(20);
+    // There now should be 20 cells
+    expect(mainPage.cells.count()).toBe(20);
 
     // Reset state
-    linesNb.clear()
-    linesNb.sendKeys('10');
-    columnsNb.clear()
-    columnsNb.sendKeys('10');
+    mainPage.resetDimensions();
 
   });
 
   it('should focus on next input when typing into an input', function() {
 
-    var firstInput = element(by.css('#grid tr:nth-child(1) td:nth-child(9) input'));
-    var secondInput = element(by.css('#grid tr:nth-child(1) td:nth-child(10) input'));
-    var thirdInput = element(by.css('#grid tr:nth-child(2) td:nth-child(1) input'));
-
     // Type into the first input (first line, penultimate column)
-    firstInput.sendKeys('A');
-    expect(firstInput.getAttribute('value')).toBe('A');
+    mainPage.getCell(1, 9).sendKeys('A');
+    expect(mainPage.getCell(1, 9).getAttribute('value')).toBe('A');
 
     // Type another letter and verify it was send to the second input (first line, last column)
     browser.actions().sendKeys('B').perform();
-    expect(secondInput.getAttribute('value')).toBe('B');
+    expect(mainPage.getCell(1, 10).getAttribute('value')).toBe('B');
 
     // Type another letter and verify it was send to the thrid input (second line, first column)
     browser.actions().sendKeys('C').perform();
-    expect(thirdInput.getAttribute('value')).toBe('C');
+    expect(mainPage.getCell(2, 1).getAttribute('value')).toBe('C');
 
     // Reset state
-    firstInput.clear();
-    secondInput.clear();
-    thirdInput.clear();
+    mainPage.getCell(1, 9).clear();
+    mainPage.getCell(1, 10).clear();
+    mainPage.getCell(2, 1).clear();
 
   });
 
   it('should save the grid dimensions and data and load them on next visit', function() {
 
-    var linesNb = element(by.model('linesNb'));
-    var columnsNb = element(by.model('columnsNb'));
-
     // Setting linesNb and columnsNb
-    linesNb.clear();
-    linesNb.sendKeys('4');
-    columnsNb.clear();
-    columnsNb.sendKeys('5');
+    mainPage.setLinesNumber('4');
+    mainPage.setColumnsNumber('5');
 
     // Setting some data in first cells
-    var firstInput = element(by.css('#grid tr:nth-child(1) td:nth-child(1) input'));
-    var secondInput = element(by.css('#grid tr:nth-child(2) td:nth-child(3) input'));
-    var thirdInput = element(by.css('#grid tr:nth-child(4) td:nth-child(2) input'));
-    firstInput.sendKeys('A');
-    secondInput.sendKeys('B');
-    thirdInput.sendKeys('C');
+    mainPage.getCell(1, 1).sendKeys('A');
+    mainPage.getCell(2, 3).sendKeys('B');
+    mainPage.getCell(4, 2).sendKeys('C');
 
     // Saving the grid
-    var saveButton = element(by.css("#save-grid"));
-    saveButton.click();
+    mainPage.saveGridButton.click();
 
     // Changing the dimensions and data
-    linesNb.clear();
-    linesNb.sendKeys('2');
-    columnsNb.clear();
-    columnsNb.sendKeys('2');
-    firstInput.clear();
-    firstInput.sendKeys('D');
+    mainPage.setLinesNumber('2');
+    mainPage.setColumnsNumber('2');
+    mainPage.getCell(1, 1).sendKeys('D');
 
     // Loading the saved grid
-    var loadButton = element(by.css("#load-grid"));
-    loadButton.click();
+    mainPage.loadGridButton.click();
 
     // The dimensions and data should be loaded
-    expect(linesNb.getAttribute('value')).toBe('4');
-    expect(columnsNb.getAttribute('value')).toBe('5');
-    expect(firstInput.getAttribute('value')).toBe('A');
-    expect(secondInput.getAttribute('value')).toBe('B');
-    expect(thirdInput.getAttribute('value')).toBe('C');
+    expect(mainPage.linesNbInput.getAttribute('value')).toBe('4');
+    expect(mainPage.columnsNbInput.getAttribute('value')).toBe('5');
+    expect(mainPage.getCell(1, 1).getAttribute('value')).toBe('A');
+    expect(mainPage.getCell(2, 3).getAttribute('value')).toBe('B');
+    expect(mainPage.getCell(4, 2).getAttribute('value')).toBe('C');
 
-    // Reset
-    firstInput.clear();
-    secondInput.clear();
-    thirdInput.clear();
-    linesNb.clear()
-    linesNb.sendKeys('10');
-    columnsNb.clear()
-    columnsNb.sendKeys('10');
+    // Reset state
+    mainPage.getCell(1, 1).clear();
+    mainPage.getCell(2, 3).clear();
+    mainPage.getCell(4, 2).clear();
+    mainPage.resetDimensions();
 
   });
 
